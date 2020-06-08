7b6a50e4b7d1b5d4103253d2ff762ceb9d2bc483
Talwinder Singh <singhtalwinder790@gmail.com>
2020-06-08 16:22:19 +0530

    Improved UI

--- client/src/components/fileUploader/FileUploader.js
+++ client/src/components/fileUploader/FileUploader.js
@@ -1,12 +1,22 @@
-import React, { useState, Fragment } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import ProgressBar from "../progressBar/ProgressBar";
 import "./FileUploader.css";
 
 export default function FileUploader() {
-	const [file, setFile] = useState();
+	const [file, setFile] = useState({
+		name: "Choose a file",
+	});
+
+	const [progress, setProgress] = useState(0);
 
 	const handleFileChange = (event) => {
 		setFile(event.target.files[0]);
+		console.log(file);
+	};
+
+	const handleBrowse = () => {
+		document.getElementById("inputFile").click();
 	};
 
 	const upload = async () => {
@@ -14,16 +24,35 @@ export default function FileUploader() {
 		data.append("file", file);
 
 		try {
-			const response = await axios.post("/upload", data, {});
+			const response = await axios.post("/upload", data, {
+				headers: { "Content-Type": "multipart/form-data" },
+				onUploadProgress: (progressEvent) => {
+					setProgress(
+						Math.round((progressEvent.loaded * 100) / progressEvent.total)
+					);
+				},
+			});
 			alert(response.data);
 		} catch (err) {
 			alert(err.response.data);
 		}
 	};
 	return (
-		<Fragment>
-			<input type="file" name="file" onChange={handleFileChange} />
-			<button onClick={upload}>upload</button>
-		</Fragment>
+		<div className="container">
+			<input
+				id="inputFile"
+				type="file"
+				name="file"
+				onChange={handleFileChange}
+			/>
+			<div className="fileName">{file.name}</div>
+			<button className="browse" onClick={handleBrowse}>
+				Browse
+			</button>
+			<button className="upload" onClick={upload}>
+				Upload
+			</button>
+			<ProgressBar progress={progress} />
+		</div>
 	);
 }
